49c1d81723db84df045f9d6c1e97747ba012018c
jnguiot <45011516+jnguiot@users.noreply.github.com>
2026-02-13 15:04:46 -1000

    Fix cache_mtime

--- flask_images/core.py
+++ flask_images/core.py
@@ -435,7 +435,7 @@ class Images(object):
             image.save(cache_file, format, quality=quality)
             cache_file.close()
         
-        return send_file(cache_path, mimetype=mimetype, max_age=cache_timeout)
+        return send_file(cache_path, mimetype=mimetype, max_age=cache_mtime)
 
 
 
